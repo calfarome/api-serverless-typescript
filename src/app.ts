751956a0cc28326f8e43d48a5ba78368a5b61692
calfaro <cesarin28am@gmail.com>
2023-08-28 11:21:00 -0500

    docs with Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { json, urlencoded, Request as ExRequest, Response as ExResponse, NextFunction, Application } from "express";
 import { RegisterRoutes } from "../dist/routes";
 import {errorHandler} from '../src/errorHandler'
+import swaggerUi from "swagger-ui-express";
 
 // exporto la aplicacion
 export const app: Application = express();
@@ -8,6 +9,11 @@ export const app: Application = express();
 // middleware
 app.use( urlencoded({extended: true,}),);
 app.use(json());
+
+// Documentación
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(swaggerUi.generateHTML(await import("../dist/swagger.json")));
+});
 
 // Registro rutas generados por tsoa
 RegisterRoutes(app);
